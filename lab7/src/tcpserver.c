0da3f23d49f7dbc4f889d6fa047601758f6c1f89
delilar <145987873+delilar@users.noreply.github.com>
2024-11-25 14:23:02 +0000

    Выполнил ЛР-7

--- lab7/src/tcpserver.c
+++ lab7/src/tcpserver.c
@@ -2,61 +2,59 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-
 #include <sys/socket.h>
 #include <sys/types.h>
 #include <unistd.h>
 
-#define SERV_PORT 10050
-#define BUFSIZE 100
-#define SADDR struct sockaddr
+#define DEFAULT_PORT 10050
+#define DEFAULT_BUFSIZE 100
 
-int main() {
-  const size_t kSize = sizeof(struct sockaddr_in);
+int main(int argc, char *argv[]) {
+    int port = argc > 1 ? atoi(argv[1]) : DEFAULT_PORT;
+    int bufsize = argc > 2 ? atoi(argv[2]) : DEFAULT_BUFSIZE;
+    char buf[bufsize];
 
-  int lfd, cfd;
-  int nread;
-  char buf[BUFSIZE];
-  struct sockaddr_in servaddr;
-  struct sockaddr_in cliaddr;
+    int lfd, cfd, nread;
+    struct sockaddr_in servaddr, cliaddr;
+    const size_t kSize = sizeof(struct sockaddr_in);
 
-  if ((lfd = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
-    perror("socket");
-    exit(1);
-  }
-
-  memset(&servaddr, 0, kSize);
-  servaddr.sin_family = AF_INET;
-  servaddr.sin_addr.s_addr = htonl(INADDR_ANY);
-  servaddr.sin_port = htons(SERV_PORT);
-
-  if (bind(lfd, (SADDR *)&servaddr, kSize) < 0) {
-    perror("bind");
-    exit(1);
-  }
-
-  if (listen(lfd, 5) < 0) {
-    perror("listen");
-    exit(1);
-  }
-
-  while (1) {
-    unsigned int clilen = kSize;
-
-    if ((cfd = accept(lfd, (SADDR *)&cliaddr, &clilen)) < 0) {
-      perror("accept");
-      exit(1);
-    }
-    printf("connection established\n");
-
-    while ((nread = read(cfd, buf, BUFSIZE)) > 0) {
-      write(1, &buf, nread);
+    if ((lfd = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
+        perror("socket");
+        exit(1);
     }
 
-    if (nread == -1) {
-      perror("read");
-      exit(1);
+    memset(&servaddr, 0, kSize);
+    servaddr.sin_family = AF_INET;
+    servaddr.sin_addr.s_addr = htonl(INADDR_ANY);
+    servaddr.sin_port = htons(port);
+
+    if (bind(lfd, (struct sockaddr *)&servaddr, kSize) < 0) {
+        perror("bind");
+        exit(1);
     }
-    close(cfd);
-  }
+
+    if (listen(lfd, 5) < 0) {
+        perror("listen");
+        exit(1);
+    }
+
+    while (1) {
+        unsigned int clilen = kSize;
+
+        if ((cfd = accept(lfd, (struct sockaddr *)&cliaddr, &clilen)) < 0) {
+            perror("accept");
+            exit(1);
+        }
+        printf("connection established\n");
+
+        while ((nread = read(cfd, buf, bufsize)) > 0) {
+            write(1, &buf, nread);
+        }
+
+        if (nread == -1) {
+            perror("read");
+            exit(1);
+        }
+        close(cfd);
+    }
 }
--- lab7/src/udpserver.c
+++ lab7/src/udpserver.c
@@ -4,53 +4,52 @@
 #include <stdlib.h>
 #include <string.h>
 #include <sys/socket.h>
-#include <sys/stat.h>
-#include <sys/types.h>
 #include <unistd.h>
 
-#define SERV_PORT 20001
-#define BUFSIZE 1024
-#define SADDR struct sockaddr
-#define SLEN sizeof(struct sockaddr_in)
+#define DEFAULT_PORT 20001
+#define DEFAULT_BUFSIZE 1024
 
-int main() {
-  int sockfd, n;
-  char mesg[BUFSIZE], ipadr[16];
-  struct sockaddr_in servaddr;
-  struct sockaddr_in cliaddr;
+int main(int argc, char *argv[]) {
+    int port = argc > 1 ? atoi(argv[1]) : DEFAULT_PORT;
+    int bufsize = argc > 2 ? atoi(argv[2]) : DEFAULT_BUFSIZE;
 
-  if ((sockfd = socket(AF_INET, SOCK_DGRAM, 0)) < 0) {
-    perror("socket problem");
-    exit(1);
-  }
+    char mesg[bufsize], ipadr[16];
+    int sockfd, n;
+    struct sockaddr_in servaddr, cliaddr;
 
-  memset(&servaddr, 0, SLEN);
-  servaddr.sin_family = AF_INET;
-  servaddr.sin_addr.s_addr = htonl(INADDR_ANY);
-  servaddr.sin_port = htons(SERV_PORT);
-
-  if (bind(sockfd, (SADDR *)&servaddr, SLEN) < 0) {
-    perror("bind problem");
-    exit(1);
-  }
-  printf("SERVER starts...\n");
-
-  while (1) {
-    unsigned int len = SLEN;
-
-    if ((n = recvfrom(sockfd, mesg, BUFSIZE, 0, (SADDR *)&cliaddr, &len)) < 0) {
-      perror("recvfrom");
-      exit(1);
+    if ((sockfd = socket(AF_INET, SOCK_DGRAM, 0)) < 0) {
+        perror("socket problem");
+        exit(1);
     }
-    mesg[n] = 0;
 
-    printf("REQUEST %s      FROM %s : %d\n", mesg,
-           inet_ntop(AF_INET, (void *)&cliaddr.sin_addr.s_addr, ipadr, 16),
-           ntohs(cliaddr.sin_port));
+    memset(&servaddr, 0, sizeof(servaddr));
+    servaddr.sin_family = AF_INET;
+    servaddr.sin_addr.s_addr = htonl(INADDR_ANY);
+    servaddr.sin_port = htons(port);
 
-    if (sendto(sockfd, mesg, n, 0, (SADDR *)&cliaddr, len) < 0) {
-      perror("sendto");
-      exit(1);
+    if (bind(sockfd, (struct sockaddr *)&servaddr, sizeof(servaddr)) < 0) {
+        perror("bind problem");
+        exit(1);
     }
-  }
+
+    printf("SERVER starts...\n");
+
+    while (1) {
+        unsigned int len = sizeof(cliaddr);
+
+        if ((n = recvfrom(sockfd, mesg, bufsize, 0, (struct sockaddr *)&cliaddr, &len)) < 0) {
+            perror("recvfrom problem");
+            exit(1);
+        }
+        mesg[n] = 0;
+
+        printf("REQUEST %s FROM %s : %d\n", mesg,
+               inet_ntop(AF_INET, &cliaddr.sin_addr, ipadr, 16),
+               ntohs(cliaddr.sin_port));
+
+        if (sendto(sockfd, mesg, n, 0, (struct sockaddr *)&cliaddr, len) < 0) {
+            perror("sendto problem");
+            exit(1);
+        }
+    }
 }
